5847093015f278403385dc0339e492614e7fa99c
nish-code <nishanth.prabhu@glassbeam.com>
2019-07-04 17:51:28 +0530

    first and final commit

---
+++ src/app/components/todo-item/todo-item.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Todo } from 'src/app/models/Todo';
+import { TodoService } from '../../services/todo.service' 
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css']
+})
+export class TodoItemComponent implements OnInit {
+  @Input() todo: Todo;
+  @Output() deleteTodo: EventEmitter<Todo> = new EventEmitter();
+ 
+  constructor(private todoService:TodoService) { }
+
+  ngOnInit() {
+  }
+
+  //set dynamic classes
+  setClasses(){
+    let classes = {
+      todo: true,
+      'is-complete': this.todo.completed
+    }
+    return classes;
+  }
+
+  onToggle(todo){ 
+    //toggle in ui
+    todo.completed = !todo.completed;
+    //toggle in server
+    this.todoService.togglecompleted(todo).subscribe(todo => 
+      console.log(todo))
+
+  }
+  onDelete(todo){
+    
+this.deleteTodo.emit(todo);
+  }
+}
